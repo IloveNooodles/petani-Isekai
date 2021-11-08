72b2254f446788d33d049fe5a1c0a451f026804f
FelineJTD <16520335@std.stei.itb.ac.id>
2021-11-08 22:09:09 +0700

    add startGame

---
+++ db.pl
@@ -0,0 +1,17 @@
+/* START GAME */
+startGame:- 
+    write('             x\n  .-. _______|\n  |=|/     /  \\ \n  | |_____|_""_|\n  |_|_[X]_|____|\n\n'), /* ASCII ART */
+    write('Harvest Galaxy S22!!!\n'),
+    write('Let\'s play and pay our debts together!\n\n'),
+    write('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n'),
+    write('%                           ~Harvest Galaxy S22~                               %\n'),
+    write('% 1. start.  : untuk memulai petualanganmu                                     %\n'),
+    write('% 2. map.    : menampilkan peta                                                %\n'),
+    write('% 3. status. : menampilkan kondisimu terkini                                   %\n'),
+    write('% 4. w.      : gerak ke utara 1 langkah                                        %\n'),
+    write('% 5. s.      : gerak ke selatan 1 langkah                                      %\n'),
+    write('% 6. d.      : gerak ke ke timur 1 langkah                                     %\n'),
+    write('% 7. a.      : gerak ke barat 1 langkah                                        %\n'),
+    write('% 8. help.   : menampilkan bantuan                                             %\n'),
+    write('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n\n'),
+    write('PENTING!!!\nAkhiri semua perintahmu dengan titik (.)').
