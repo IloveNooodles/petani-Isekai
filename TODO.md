f0681041b266cf61f59a2c79c3b9e4d907b23779
IloveNooodles <mgarebaldhie80@gmail.com>
2021-11-17 14:09:56 +0700

    items WIP

--- TODO.md
+++ TODO.md
@@ -1,22 +1,28 @@
-Feli 
+Feli
+
 1. Pemain
 2. Exploration
 3. Farming
 
 Adit
+
 1. Peta
 2. Inventory
 3. MANTAPANJING
 
 Afan
+
 1. Quests
 2. Berkuda
 3. Market Place
 
 Gare
+
 1. Items
 2. House
 3. Endgame
 
 Changelog:
-- 
+
+- Add startGame, start, baseStats, ResetStats, Status by Feli
+-
---
+++ src/items.pl
@@ -0,0 +1,74 @@
+/* Item bakal ada ada 2 arity nama sama tipe */
+
+/* Consumable */
+/* ini bisa dimakan dan buat nambah stamina possibly, dan bakal ada restoran */
+item(energyDrink, consumable).
+item(kopiCampur, consumable).
+item(susuSegar, consumable).
+item(milkshake, consumable).
+item(crisbar, consumable).
+item(steak, consumable).
+
+
+/* Farming */
+item(padi, farming).
+item(benihSurga, farming).
+item(bibitMisterius, farming).
+item(cocoaSeeds, farming).
+item(cucumberSeeds, farming).
+item(tomatoSeeds, farming).
+item(carrotSeeds, farming).
+item(coffeeSeeds, farming).
+item(bijiKopi, farming).
+item(carrot, farming).
+item(cokelat, farming).
+item(cucumber, farming).
+item(tomato, farming).
+item(garlic, farming).
+item(bibitDunia, famring).
+item(herb, farming).
+
+/* Ranching */
+item(chicken, ranching).
+item(goldenChicken, ranching).
+item(cow, ranching).
+item(cowWagyu, ranching).
+item(sheep, ranching).
+item(pinkSheep, ranching).
+item(wool, ranching).
+item(egg, ranching).
+item(goldenEgg, ranching).
+item(milk, ranching).
+item(chickenMeat, ranching).
+item(sheepMeat, ranching).
+item(cowRib, ranching).
+item(cowLoin, ranching).
+item(cowBrisket, ranching).
+
+/* Fishing */
+item(tuna, fishing).
+item(salmon, fishing).
+item(angler, fishing).
+item(crimsonfish, fishing).
+item(sunfish, fishing).
+item(relicanth, fishing).
+item(kingfish, fishing).
+item(mutant, fishing).
+item(lobster, fishing).
+item(sardine, fishing).
+item(catfish, fishing).
+item(octopus, fishing).
+item(eel, fishing).
+item(seacucumber, fishing).
+item(ghostfish, fishing).
+item(stonefish, fishing).
+item(sashimi, fishing).
+item(sushi, fishing).
+
+/* Tools */
+/* Buat bagian tools ini bakal ada nama, tipe, sama level */
+item(shovel, tools, level).
+item(shear, tools, level).
+item(fishingRod, tools, level).
+item(wateringcan, tools, level).
+item(catchingNet, tools, level).
