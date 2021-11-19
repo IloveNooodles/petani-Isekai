dd9b84287232fe127d8bec894a0293966403b6cb
Adityapnn811 <72344203+Adityapnn811@users.noreply.github.com>
2021-11-19 20:11:22 +0700

    Membuat mekanisme map dan jalan jalan

--- src/main.pl
+++ src/main.pl
@@ -3,5 +3,13 @@
 
 /* INCLUDES */
 :- include('startGame.pl').
+:- include('map.pl').
 
 /* START GAME */
+lesgo :-
+    asserta(player_name(charlie)),
+    write('Siapa namamu, calon petani?\n'),
+    read(Name),
+    retractall(player_name(_)),
+    setPlayerName(Name),
+    fill_map.
---
+++ src/map.pl
@@ -0,0 +1,357 @@
+/* Spek Map
+Peta berukuran 20x20, dengan tambahan 2 kolom dan 2 baris untuk border
+(X, Y) itu koordinat di map
+pojok kiri atas itu (1,1)
+Pagar di koordinat (1,Y), (X, 1), (20, Y), (X,20)
+*/
+
+/* dynamic predicate */
+baris(22).
+kolom(22).
+:- dynamic(border/2). /* koordinat border */
+:- dynamic(player/2). /* koordinat border */
+:- dynamic(market_coordinate/2). /* koordinat border */
+:- dynamic(home_coordinate/2). /* koordinat border */
+:- dynamic(ranch_coordinate/2). /* koordinat border */
+:- dynamic(quest_coordinate/2). /* koordinat border */
+:- dynamic(dirt/2). /* koordinat border */
+:- dynamic(digged_coordinate/2). /* koordinat border */
+:- dynamic(water_coordinate/2). /* koordinat border */
+:- dynamic(loc_tile/1).
+:- dynamic(player_name/1). /* ini sementara */
+
+/* Spek Map
+border = #
+player = P
+market = M
+home = H
+ranch = R
+quest = Q
+dirt = -
+digged = =
+water = o
+*/
+
+/*
+Ada predikat loc_tile untuk menyatakan situasi lokasi player, seperti:
+home: player masuk home
+quest: playert masuk quest
+ranch: player masuk ranch
+market: player masuk market
+digged: player di kebun
+lake_edge: player di pinggir danau
+*/
+
+/* Map */
+fill_map :- 
+    /* hapus dulu map sebelomnya */
+    retractall(border(_,_)),
+    retractall(player(_,_)),
+    retractall(market_coordinate(_,_)),
+    retractall(home_coordinate(_,_)),
+    retractall(ranch_coordinate(_,_)),
+    retractall(quest_coordinate(_,_)),
+    retractall(dirt(_,_)),
+    retractall(digged_coordinate(_,_)),
+    retractall(water_coordinate(_,_)),
+    /* buat map */
+    % Border map
+    asserta(border(1,_)),
+    asserta(border(_,1)),
+    asserta(border(22,_)),
+    asserta(border(_,22)),
+    % Player
+    assertz(player(17,5)),
+    % market
+    asserta(market_coordinate(18,7)),
+    % home
+    asserta(home_coordinate(12,6)),
+    % ranch
+    asserta(ranch_coordinate(12,8)),
+    % quest
+    asserta(quest_coordinate(18,5)),
+    % digged (ini kayanya harus di hardcode)
+    % asserta(digged_coordinate(X,Y)),
+    % water deket home
+    asserta(water_coordinate(5,9)),
+    asserta(water_coordinate(6,9)),
+    asserta(water_coordinate(7,9)),
+    asserta(water_coordinate(4,10)),
+    asserta(water_coordinate(5,10)),
+    asserta(water_coordinate(6,10)),
+    asserta(water_coordinate(7,10)),
+    asserta(water_coordinate(8,10)),
+    asserta(water_coordinate(4,11)),
+    asserta(water_coordinate(5,11)),
+    asserta(water_coordinate(6,11)),
+    asserta(water_coordinate(7,11)),
+    asserta(water_coordinate(8,11)),
+    asserta(water_coordinate(5,12)),
+    asserta(water_coordinate(6,12)),
+    asserta(water_coordinate(7,12)),
+    % water pojok kiri bawah
+    asserta(water_coordinate(4,18)),
+    asserta(water_coordinate(5,18)),
+    asserta(water_coordinate(6,18)),
+    asserta(water_coordinate(7,18)),
+    asserta(water_coordinate(3,19)),
+    asserta(water_coordinate(4,19)),
+    asserta(water_coordinate(5,19)),
+    asserta(water_coordinate(6,19)),
+    asserta(water_coordinate(7,19)),
+    asserta(water_coordinate(8,19)),
+    asserta(water_coordinate(3,20)),
+    asserta(water_coordinate(4,20)),
+    asserta(water_coordinate(5,20)),
+    asserta(water_coordinate(6,20)),
+    asserta(water_coordinate(7,20)),
+    asserta(water_coordinate(8,20)),
+    asserta(water_coordinate(4,21)),
+    asserta(water_coordinate(5,21)),
+    asserta(water_coordinate(6,21)),
+    asserta(water_coordinate(7,21)),
+    % isi dengan dirt
+    asserta(dirt(_,_)),
+    % deklarasi loc tile
+    asserta(loc_tile(_)).
+
+% Set player name
+setPlayerName(Name) :-
+    asserta(player_name(Name)).
+
+% Debugging loc_check
+print_loc_now :- player_name(Name), loc_tile(quest), !, format('~w bisa mengambil quest!', [Name]), nl.
+print_loc_now :- player_name(Name), loc_tile(market), !, format('~w masuk ke Marketplace!', [Name]), nl.
+print_loc_now :- player_name(Name), loc_tile(home), !, format('~w masuk ke Home!', [Name]), nl.
+print_loc_now :- player_name(Name), loc_tile(ranch), !, format('~w masuk ke Ranch!', [Name]), nl.
+print_loc_now :- player_name(Name), loc_tile(digged), !, format('~w sedang mengecek kebun!', [Name]), nl.
+print_loc_now :- player_name(Name), loc_tile(lake_edge), !, format('~w di pinggir danau!', [Name]), nl.
+
+/* print karakter */
+printChar(X, Y) :- border(X, Y), !, write('#').
+printChar(X, Y) :- player(X, Y), !, write('P').
+printChar(X, Y) :- market_coordinate(X, Y), !, write('M').
+printChar(X, Y) :- home_coordinate(X, Y), !, write('H').
+printChar(X, Y) :- ranch_coordinate(X, Y), !, write('R').
+printChar(X, Y) :- quest_coordinate(X, Y), !, write('Q').
+printChar(X, Y) :- digged_coordinate(X, Y), !, write('=').
+printChar(X, Y) :- water_coordinate(X, Y), !, write('o').
+printChar(X, Y) :- dirt(X, Y), !, write('-').
+
+% kayanya harus liat state gamenya
+map :-
+    forall(between(1,22,Y),(
+        forall(between(1,22,X), (
+            printChar(X,Y)
+        )),
+        nl
+    )).
+
+% Fungsi cek lokasi sekarang
+% Cek di market
+loc_check(X,Y) :- 
+    market_coordinate(X,Y),
+    !,
+    retractall(loc_tile(_)),
+    asserta(loc_tile(market)),
+    print_loc_now.
+%  cek di home
+loc_check(X,Y) :- 
+    home_coordinate(X,Y),
+    !,
+    retractall(loc_tile(_)),
+    asserta(loc_tile(home)),
+    print_loc_now.
+
+% cek di ranch
+loc_check(X,Y) :- 
+    ranch_coordinate(X,Y),
+    !,
+    retractall(loc_tile(_)),
+    asserta(loc_tile(ranch)),
+    print_loc_now.
+    
+% cek di Quest
+loc_check(X,Y) :- 
+    quest_coordinate(X,Y),
+    !,
+    retractall(loc_tile(_)),
+    asserta(loc_tile(quest)),
+    print_loc_now.
+
+% cek di digged tile
+loc_check(X,Y) :- 
+    digged_coordinate(X,Y),
+    !,
+    retractall(loc_tile(_)),
+    asserta(loc_tile(market)),
+    print_loc_now.
+    
+% Cek kalo dia di pinggir danau, ada 8 kasus, player di kanan, kiri, atas, bawah, trus di serong kanan atas, dst
+loc_check(X,Y) :-
+    XAround is X + 1,
+    water_coordinate(XAround,Y),
+    !,
+    retractall(loc_tile(_)),
+    asserta(loc_tile(lake_edge)),
+    print_loc_now.
+   
+loc_check(X,Y) :-
+    XAround is X - 1,
+    water_coordinate(XAround,Y),
+    !,
+    retractall(loc_tile(_)),
+    asserta(loc_tile(lake_edge)),
+    print_loc_now.
+   
+loc_check(X,Y) :-
+    YAround is Y - 1,
+    water_coordinate(X,YAround),
+    !,
+    retractall(loc_tile(_)),
+    asserta(loc_tile(lake_edge)),
+    print_loc_now.
+
+loc_check(X,Y) :-
+    YAround is Y + 1,
+    water_coordinate(X,YAround),
+    !,
+    retractall(loc_tile(_)),
+    asserta(loc_tile(lake_edge)),
+    print_loc_now.
+% Kasus serong-serong
+loc_check(X,Y) :-
+    XAround is X + 1,
+    YAround is Y + 1,
+    water_coordinate(XAround,YAround),
+    !,
+    retractall(loc_tile(_)),
+    asserta(loc_tile(lake_edge)),
+    print_loc_now.
+
+loc_check(X,Y) :-
+    XAround is X + 1,
+    YAround is Y - 1,
+    water_coordinate(XAround,YAround),
+    !,
+    retractall(loc_tile(_)),
+    asserta(loc_tile(lake_edge)),
+    print_loc_now.
+
+loc_check(X,Y) :-
+    XAround is X - 1,
+    YAround is Y + 1,
+    water_coordinate(XAround,YAround),
+    !,
+    retractall(loc_tile(_)),
+    asserta(loc_tile(lake_edge)),
+    print_loc_now.
+
+loc_check(X,Y) :-
+    XAround is X - 1,
+    YAround is Y - 1,
+    water_coordinate(XAround,YAround),
+    !,
+    retractall(loc_tile(_)),
+    asserta(loc_tile(lake_edge)),
+    print_loc_now.
+
+% Buat gerak gerak
+% Buat w
+w :- 
+    player_name(Name),
+    player(X,Y),
+    Ynow is Y - 1,
+    border(X,Ynow),
+    !,
+    format('~w nabrak pagar :(', [Name]), nl.
+
+w :- 
+    player_name(Name),
+    player(X,Y),
+    Ynow is Y - 1,
+    water_coordinate(X,Ynow),
+    !,
+    format('~w hampir aja masuk danau, kasian ntar tenggelam, dia gabisa renang.', [Name]), nl.
+
+w :- 
+    player_name(Name),
+    player(X,Y),
+    Ynow is Y - 1,
+    retractall(player(_,_)),
+    asserta(player(X, Ynow)),
+    !, format('~w bergerak ke utara satu langkah!', [Name]), nl, loc_check(X,Ynow), print_loc_now.
+% Buat a
+a :- 
+    player_name(Name),
+    player(X,Y),
+    Xnow is X-1,
+    border(Xnow,Y),
+    !,
+    format('~w nabrak pagar :(', [Name]), nl.
+
+a :- 
+    player_name(Name),
+    player(X,Y),
+    Xnow is X-1,
+    water_coordinate(Xnow,Y),
+    !,
+    format('~w hampir aja masuk danau, kasian ntar tenggelam, dia gabisa renang.', [Name]), nl.
+
+a :- 
+    player_name(Name),
+    player(X,Y),
+    Xnow is X - 1,
+    retractall(player(_,_)),
+    asserta(player(Xnow, Y)),
+    !, format('~w bergerak ke barat satu langkah!', [Name]), nl, loc_check(Xnow,Y).
+
+% Buat s
+s :- 
+    player_name(Name),
+    player(X,Y),
+    Ynow is Y + 1,
+    border(X,Ynow),
+    !,
+    format('~w nabrak pagar :(', [Name]), nl.
+
+s :- 
+    player_name(Name),
+    player(X,Y),
+    Ynow is Y + 1,
+    water_coordinate(X,Ynow),
+    !,
+    format('~w hampir aja masuk danau, kasian ntar tenggelam, dia gabisa renang.', [Name]), nl.
+
+s :- 
+    player_name(Name),
+    player(X,Y),
+    Ynow is Y + 1,
+    retractall(player(_,_)),
+    asserta(player(X, Ynow)),
+    !, format('~w bergerak ke selatan satu langkah!', [Name]), nl, loc_check(X,Ynow).
+
+% Buat d
+d :- 
+    player_name(Name),
+    player(X,Y),
+    Xnow is X+1,
+    water_coordinate(Xnow,Y),
+    !,
+    format('~w hampir aja masuk danau, kasian ntar tenggelam, dia gabisa renang.', [Name]), nl.
+
+d :- 
+    player_name(Name),
+    player(X,Y),
+    Xnow is X+1,
+    border(Xnow,Y),
+    !,
+    format('~w nabrak pagar :(', [Name]), nl.
+
+d :- 
+    player_name(Name),
+    player(X,Y),
+    Xnow is X + 1,
+    retractall(player(_,_)),
+    asserta(player(Xnow, Y)),
+    !, format('~w bergerak ke timur satu langkah!', [Name]), nl, loc_check(Xnow,Y).
