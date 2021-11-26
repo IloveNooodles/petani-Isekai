f42c4ee62bfb3c4ee54fcd26e29da0fd4158c4a1
Adityapnn811 <72344203+Adityapnn811@users.noreply.github.com>
2021-11-26 17:01:23 +0700

    Menambahkan beberapa ikan jadi total ikan 16, melanjutkan mekanik fishing, membuat fungsi proses input bait

--- src/fishing.pl
+++ src/fishing.pl
@@ -14,15 +14,19 @@ waktu random klo dapet 0 berarti ndak dapet apa apa
 ikan(tuna, 1, 1).
 ikan(sardine, 1, 2).
 ikan(eel, 1, 3).
+ikan(rareEel, 1, 4).
 ikan(catfish, 2, 1).
 ikan(seacucumber, 2, 2).
 ikan(stonefish, 2, 3).
+ikan(rareStonefish, 2, 4).
 ikan(octopus, 3, 1).
 ikan(salmon, 3, 2).
 ikan(ghostfish, 3, 3).
+ikan(rareGhostfish, 3, 4).
 ikan(kingfish, 4, 1).
 ikan(lobster, 4, 2).
 ikan(mutant, 4, 3).
+ikan(rareMutant, 4, 4).
 ikan(sunfish, 5, 1).
 ikan(crimsonfish, 5, 2).
 ikan(angler, 5, 3).
@@ -55,5 +59,106 @@ getFish(Level, Rare, _Job) :-
     !,
     write('Kamu mendapatkan 10 exp fishing!\n').
 
+% Buat ngeproses input bait
+% buat bait
+baitInput(1) :-
+    inventory(List, _),
+    countXinInven(bait, List, Countbait),
+    Countbait = 0,
+    !,
+    write('Kamu gapunya bait!\n').
+% buat bait biasa, cuma bisa dapet rarity 0 sampe 2
+baitInput(1) :-
+    job(Job),
+    level(fish, Level),
+    inventory(List, Total),
+    retractall(inventory(_,_)),
+    NewTotal is Total - 1,
+    deleteValinList(bait, List, ListOut),
+    asserta(inventory(ListOut, NewTotal)),
+    updateInvenOne(bait),
+    write('Kamu menggunakan bait!\n'),
+    random(0, 2, Hasil),
+    getFish(Level, Hasil, Job), !.
+% buat good bait
+baitInput(2) :-
+    inventory(List, _),
+    countXinInven(goodBait, List, Countbait),
+    Countbait = 0,
+    !,
+    write('Kamu gapunya good bait!\n').
+
+baitInput(2) :-
+    job(Job),
+    level(fish, Level),
+    inventory(List, Total),
+    retractall(inventory(_,_)),
+    NewTotal is Total - 1,
+    deleteValinList(goodBait, List, ListOut),
+    asserta(inventory(ListOut, NewTotal)),
+    updateInvenOne(goodBait),
+    write('Kamu menggunakan good bait!\n'),
+    random(1, 3, Hasil),
+    getFish(Level, Hasil, Job), !.
+
+% buat great bait
+baitInput(3) :-
+    inventory(List, _),
+    countXinInven(greatestBait, List, Countbait),
+    Countbait = 0,
+    !,
+    write('Kamu gapunya great bait!\n').
+
+baitInput(3) :-
+    job(Job),
+    level(fish, Level),
+    inventory(List, Total),
+    retractall(inventory(_,_)),
+    NewTotal is Total - 1,
+    deleteValinList(greatestBait, List, ListOut),
+    asserta(inventory(ListOut, NewTotal)),
+    updateInvenOne(greatestBait),
+    write('Kamu menggunakan great bait!\n'),
+    random(2, 4, Hasil),
+    getFish(Level, Hasil, Job), !.
+
 % Command fishing
-% fish :-
+fish :- % kasus ngga bisa mancing karena ngga di pinggir danau
+    \+ loc_tile(lake_edge),
+    !,
+    write('Ngga ada danau di sekitarmu!!\n').
+
+fish :- % kasus ngga bisa mancing karena ngga bawa pancingan
+    loc_tile(lake_edge),
+    \+ isXinInven(fishingRod),
+    !,
+    write('Kamu ngga bawa alat mancing!!\n').
+% Kasus ga bawa bait
+fish :-
+    loc_tile(lake_edge),
+    isXinInven(fishingRod), % belom ngecek level fishing rodnya
+    \+ isXinInven(bait),
+    \+ isXinInven(goodBait),
+    \+ isXinInven(greatestBait),
+    job(Job),
+    !,
+    random(0, 1, HasilRoll),
+    getFish(1, HasilRoll, Job).
+
+% Kasus bawa bait
+fish :-
+    inventory(List, _Total),
+    loc_tile(lake_edge),
+    isXinInven(fishingRod), % belom ngecek level fishing rodnya
+    countXinInven(bait, List, Countbait),
+    countXinInven(goodBait, List, Countgoodbait),
+    countXinInven(greatestBait, List, Countgreatbait),
+    !,
+    write('Bait yang ada di inven kamu: \n'),
+    format('1. ~d bait', [Countbait]),
+    format('2. ~d good bait', [Countgoodbait]),
+    format('3. ~d greatest bait', [Countgreatbait]),
+    read(Input),
+    baitInput(Input).
+
+% fungsi proses input bait, kek klo bait nya 0 gabisa, dll, trus
